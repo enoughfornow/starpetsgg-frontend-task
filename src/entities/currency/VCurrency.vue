4279357eaf688d216873c1da098a9ae31f85d439
Dmitry <enoughfornowbusiness@gmail.com>
2024-09-05 23:23:34 +0300

    master feat: add icons to currency rate

--- src/entities/currency/VCurrency.vue
+++ src/entities/currency/VCurrency.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
+import { UIIcon, UISelectTypes } from '@/ui';
 
 interface Props {
     value: string,
-    currency: string,
-    selectedCurrency: string
+    currency: UISelectTypes.ESelectTypes,
+    selectedCurrency: UISelectTypes.ESelectTypes
 }
 
  defineProps<Props>()
 
 const classes = {
-    container: 'flex items-center m-4',
+    container: 'flex items-center justify-between gap-2',
     text: 'font-montserrat font-size-20 flex items-center el-w-content',
+    icon: 'el-w-10 el-h-10 bg-color-gray m-4',
 }
 
 
@@ -19,9 +21,17 @@ const classes = {
 <template>
     <div
         v-if="+value"
-        :class="classes.container" >     
+        :class="classes.container" > 
+            <UIIcon 
+                :name="selectedCurrency" 
+                :class="classes.icon"
+                />    
             <p :class="classes.text">
                1 {{ selectedCurrency.toUpperCase() }} = {{ value }} {{ currency.toUpperCase() }}
             </p>
+            <UIIcon 
+                :name="currency" 
+                :class="classes.icon"
+                />   
     </div>
 </template>
