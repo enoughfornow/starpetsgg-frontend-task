ce2e419a02ba7775257f00ec43e7d694722b8c3a
awmonk <enoughfornowbusiness@gmail.com>
2024-09-06 13:56:56 +0300

    master: refactor readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
